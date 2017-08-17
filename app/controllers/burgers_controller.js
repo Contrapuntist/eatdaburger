81add76d672bd20c102d3371878dd5f3da7bbb19
Contrapuntist <contrapuntist@gmail.com>
2017-08-17 13:43:39 -0500

    router post getting set up. req from handlebars sent properply.

--- app/controllers/burgers_controller.js
+++ app/controllers/burgers_controller.js
@@ -12,16 +12,19 @@ router.get('/', function (req, res) {
     });
 });
 
-// // SQL statement: INSERT INTO table (column1, etc.) VALUES (Val for col1, etc.);
-// router.post('/:burger', function(req, res) { 
-//     // Define params for new burger
-//     // var test = req.params.burger;
-//     // sending to module 
-//     burger.create(['test'], ['test'], function() {
-//         // redirect to index with updated data 
-//         res.redirect('/');
-//     });
-// });
+// SQL statement: INSERT INTO table (column1, etc.) VALUES (Val for col1, etc.);
+router.post('/', function(req, res) { 
+    console.log(req.body.devour);
+    console.log(req.body.burgerentry); 
+    // res.redirect('/');
+    // Define params for new burger
+    // var test = req.params.burger;
+    // sending to module 
+    // burger.create(['test'], ['test'], function() {
+    //     // redirect to index with updated data 
+    //     res.redirect('/');
+    // });
+});
 
 // // SQL statement:  UPDATE table SET col = value where condition;  [condition may vary]
 // router.put('/:burger', function (req, res) {
